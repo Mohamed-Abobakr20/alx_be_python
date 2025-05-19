0611028d0cfa158a2291a2ee82ef3ecb0954f21c
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-05-19 20:35:52 +0300

    creating python_introduction folder and the basic_operations file with code for two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+# operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiply = number1 * number2 
+# [operation] of [number1] and [number2] is [result].
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
