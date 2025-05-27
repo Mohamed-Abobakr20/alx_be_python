430294df1c6b830ad484fe169890fc09c312a5d3
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-05-27 19:37:35 +0300

    changing the name of the variable weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,13 @@
 Create a Python script named weather_advice.py. This script will ask the user about the current weather conditions and provide clothing recommendations based on the input. This task aims to demonstrate the use of if, elif, and else statements to make decisions in a program
 """
 
-weather_state = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather_state == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_state == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_state == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
