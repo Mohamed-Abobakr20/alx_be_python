268cdadac99973e59792543d805024de2b6ef371
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-05-28 17:43:58 +0300

    add space in the input

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,7 +2,7 @@
 This calculator will prompt the user to enter two numbers and select an operation (addition, subtraction, multiplication, or division). The script will then perform the selected operation using a Match Case statement and display the result.
 """
 
-num1 = int(input("Enter the first number :"))
+num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
