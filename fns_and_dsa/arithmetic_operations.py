1ae476d0057fda2ec234d872a0fdf675d5befcf8
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-06-04 18:03:43 +0300

    creating folder for functions and DSA and a file for calculator function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+"""
+Create a Python script named arithmetic_operations.py. In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
+"""
+
+def perform_operation(num1, num2, operation):
+    result = ""
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide':
+            if num2 == 0:
+                result = "Cannot divide by zero"
+            else:
+                result = num1 / num2
+    return result
