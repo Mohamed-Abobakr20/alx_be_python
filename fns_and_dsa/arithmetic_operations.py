d1a57638dc6a30a349805a837141cd978a3077d2
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-06-04 18:24:12 +0300

    using if conditions instead of match case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,18 +2,19 @@
 Create a Python script named arithmetic_operations.py. In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
 """
 
+# arithmetic_operations.py
+
 def perform_operation(num1, num2, operation):
-    result = ""
-    match operation:
-        case 'add':
-            result = num1 + num2
-        case 'subtract':
-            result = num1 - num2
-        case 'multiply':
-            result = num1 * num2
-        case 'divide':
-            if num2 == 0:
-                return "Cannot divide by zero"
-            else:
-                result = num1 / num2
-    return result
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
