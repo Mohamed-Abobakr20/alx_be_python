0d99767544805367fc70db14b5df63a6082f3759
Mohamed Abobakr <abobakrmohamed2222@gmail.com>
2025-06-05 23:48:19 +0300

    oprimize the code

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,17 +1,15 @@
 """
 Create a Python script named shopping_list_manager.py that implements a simple interface for managing a shopping list. This task focuses on using lists to store and manipulate data dynamically.
 """
-def check_input(dataType):
-    match dataType:
+def check_input():
+    message = input("Choose the type you want to add (int - string - decimal)?: ")
+    item = input("Enter the item to add: ")
+    match message:
         case 'int':
-            item = int(input("Enter the item to add: "))
-            return item
-        case 'str':
-            item = input("Enter the item to add: ")
-            return item
+            item = int(item)
         case 'decimal':
-            item = float(input("Enter the item to add: "))
-            return item     
+            item = float(item)
+    return item             
             
 
 def display_menu():
@@ -30,13 +28,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            message = input("Choose the type you want to add (int - string - decimal)?: ")
-            item = check_input(message)
+            item = check_input()
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
-            message = input("Choose the type you want to add (int - string - decimal)?: ")
-            item = check_input(message)
+            
+            item = check_input()
             if item in shopping_list:
                 shopping_list.remove(item)
         elif choice == '3':
